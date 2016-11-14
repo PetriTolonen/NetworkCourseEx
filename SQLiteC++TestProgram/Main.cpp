a206a855951d87c51563f92c82c71901ac29e3ba
PetriTolonen <TTV14SPetriT@kamk.fi>
2016-11-14 16:25:07 +0200

    Uusia komentoja sql ohjelmaan.

--- SQLiteC++TestProgram/Main.cpp
+++ SQLiteC++TestProgram/Main.cpp
@@ -6,13 +6,14 @@
 
 namespace
 {
-	int playerId;
+	int currentPlayer;
 }
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 	int i;
+	
 	for (i = 0; i < argc; i++){
-		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");		
 	}
 	printf("\n");
 	return 0;
@@ -39,7 +40,7 @@ int createdatabase()
 
 	/* Create SQL statement */
 	sql = "CREATE TABLE PLAYERS("  \
-		"ID INT PRIMARY KEY     NOT NULL," \
+		"ID INT PRIMARY KEY     NOT NULL AUTOINCREMENT UNIQUE," \
 		"NAME           TEXT    NOT NULL," \
 		"SCORE			INT);";
 
@@ -61,7 +62,9 @@ void createNewPlayer()
 	sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
-	char *sql;
+	const char *sql;
+	std::string playerName;
+	std::string inputPlayer;
 
 	/* Open database */
 	rc = sqlite3_open("test.db", &db);
@@ -73,10 +76,15 @@ void createNewPlayer()
 		fprintf(stderr, "Opened database successfully\n");
 	}
 
+	std::cout << "Give player name" << std::endl;
+	std::cin >> playerName;
+
+	inputPlayer = "INSERT INTO PLAYERS (NAME,SCORE) "  \
+		"VALUES ('" + playerName + "', 0 ); " \
+		;
+
 	/* Create SQL statement */
-	sql = "INSERT INTO PLAYERS (ID,NAME,SCORE) "  \
-		  "VALUES ("+ playerId +",'Paul', 3200 ); " \ //TODO: make string and cast to char or something
-		  ;
+	sql = inputPlayer.c_str();
 
 	/* Execute SQL statement */
 	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -91,17 +99,141 @@ void createNewPlayer()
 	return;
 }
 
+void selectPlayer()
+{
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	const char *sql;
+	std::string playerName;
+	std::string selectPlayer;
+
+	/* Open database */
+	rc = sqlite3_open("test.db", &db);
+	if (rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+	else{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
+	std::cout << "Give player name to select" << std::endl;
+	std::cin >> playerName;
+
+	selectPlayer = "SELECT * FROM PLAYERS WHERE NAME = '" + playerName + "'"  \
+		;
+	/* Create SQL statement */
+	sql = selectPlayer.c_str();
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	if (rc != SQLITE_OK){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else{
+		fprintf(stdout, "Player selected successfully\n");
+	}
+	sqlite3_close(db);
+	return;
+}
+
+void updateScore()
+{
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	const char *sql;
+	std::string playerId;
+	std::string selectPlayer;
+	int score;
+
+	/* Open database */
+	rc = sqlite3_open("test.db", &db);
+	if (rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+	else{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
+	std::cout << "Give player ID to select" << std::endl;
+	std::cin >> playerId;
+
+	std::cout << "Give score for player" << std::endl;
+	std::cin >> score;
+
+	selectPlayer = "UPDATE PLAYERS set SCORE = " + std::to_string(score) + " WHERE ID = '" + playerId + "'"  \
+		;
+	/* Create SQL statement */
+	sql = selectPlayer.c_str();
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	if (rc != SQLITE_OK){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else{
+		fprintf(stdout, "Player updated successfully\n");
+	}
+	sqlite3_close(db);
+	return;
+}
+
+void high10()
+{
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	const char *sql;
+	std::string selectPlayer;
+
+	/* Open database */
+	rc = sqlite3_open("test.db", &db);
+	if (rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+	else{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
+	selectPlayer = "SELECT * FROM PLAYERS ORDER BY SCORE DESC LIMIT 10"  \
+		;
+	/* Create SQL statement */
+	sql = selectPlayer.c_str();
+
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	if (rc != SQLITE_OK){
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else{
+		fprintf(stdout, "Top 10 successfully\n");
+	}
+	sqlite3_close(db);
+	return;
+}
+
 int main(int argc, char* argv[])
 {
 	bool exiting = false;
 	while (!exiting)
 	{
-		int selection;
+		char* selection;
 		int selectionInt;
-		std::cout << "1: Create database\n2: Create new player into created Database\n3: Select from database\n4: Update Operation\n5: Delete Operation\n6: Exit" << std::endl;
+		std::cout << "1: Create database\n2: Create new player into created Database\n3: Select from database\n4: Update Score Operation\n5: Delete Operation\n6: HighScoreTop10\n7: Exit" << std::endl;
 		std::cout << "Select Task:";
-		std::cin >> selection;
+		std::cin >> selectionInt;
+
+		system("cls");
 		
+		//selectionInt = atoi(selection);
+
 		switch (selectionInt)
 		{
 		case 1:
@@ -114,8 +246,24 @@ int main(int argc, char* argv[])
 			createNewPlayer();
 			break;
 		}
+		case 3:
+		{
+			selectPlayer();
+			break;
+		}
+		case 4:
+		{
+			updateScore();
+			break;
+		}
 		case 6:
 		{
+			high10();
+			break;
+		}
+		case 7:
+		{
+			fflush(stdin);
 			char yes;
 			std::cout << "do you want to exit y/n";
 			std::cin >> yes;
